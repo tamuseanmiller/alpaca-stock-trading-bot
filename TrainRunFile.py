7c236cd31f23b93b999973b46f5f59b913107c86
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-10-19 09:53:55 -0700

    Update TrainRunFile.py

--- TrainRunFile.py
+++ TrainRunFile.py
@@ -1,8 +1,4 @@
 import os
-os.environ['APCA_API_BASE_URL'] = 'https://paper-api.alpaca.markets'
-os.environ['APCA_API_KEY_ID'] = 'PKVQCPLCNIT0LP3PCG01'
-os.environ['APCA_API_SECRET_KEY'] = '24F86p4D6CjNw0YEL4ZtcoiTpeSepcJxXccfsZH5'
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/Mohit/PycharmProjects/SuperAIPR/newslstmstockbot/keys.json'
 varvar = 0
 while True:
     print('Loop has been completed', varvar,'time(s)')
