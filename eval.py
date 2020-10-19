7af29640d3167151788a9bbbf58f4e1a59e654b6
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-10-19 10:58:15 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -14,11 +14,6 @@ Options:
 """
 
 import os
-os.environ['TF_MIN_GPU_MULTIPROCESSOR_COUNT'] = '2'
-os.environ['APCA_API_BASE_URL'] = 'https://paper-api.alpaca.markets'
-os.environ['APCA_API_KEY_ID'] = 'PKVQCPLCNIT0LP3PCG01'
-os.environ['APCA_API_SECRET_KEY'] = '24F86p4D6CjNw0YEL4ZtcoiTpeSepcJxXccfsZH5'
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/Mohit/PycharmProjects/SuperAIPR/newslstmstockbot/keys.json'
 import coloredlogs
 
 from docopt import docopt
